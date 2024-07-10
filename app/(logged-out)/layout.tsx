038989f610939c9719bbde747f1da0c5cc6f8b2d
Potato Mexicano <potatomexicano006@yahoo.com>
2024-07-09 23:06:05 -0300

    [feat] Adicionado botão para ligh & dark mode.
    * Adicionado botão com Toogle para mudar tema.

--- app/(logged-out)/layout.tsx
+++ app/(logged-out)/layout.tsx
@@ -1,11 +1,16 @@
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle"
+
 type Props = {
     children?: React.ReactNode
 }
 
 export default function loggedOutLayout({children}: Props) {
     return(
+        <>
         <div className="flex flex-col gap-4 min-h-screen items-center p-24 justify-center">
             {children}
         </div>
+            <LightDarkToggle className="fixed top-1 p-4 right-0" />
+        </>
     )
 }
