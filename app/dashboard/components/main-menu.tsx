e4e090b811f85b4766798d0e98879575a9a37085
Potato Mexicano <potatomexicano006@yahoo.com>
2024-07-16 22:22:58 -0300

    [feat] Finalizado sidepanel.
    * Finalizado implementação do SidePanel.
    * Adicionado Footer para Logout e troca do tema.

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,10 +1,30 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import MenuItem from "./menu-item";
 import MainTitle from "./menu-title";
+import Link from "next/link";
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
 
 export default function MainMenu() {
-    return <div className="bg-muted overflow-auto p-4">
+    return (
+    <nav className="bg-muted overflow-auto p-4 flex flex-col">
         <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
             <MainTitle />
         </div>
 
-    </div>
+        <div className="py-4 grow">
+            <MenuItem href="/dashboard">My dashboard</MenuItem>
+            <MenuItem href="/dashboard/employees">Teams</MenuItem>
+            <MenuItem href="/dashboard/account">Account</MenuItem>
+            <MenuItem href="/dashboard/settings">Settings</MenuItem>
+        </div>
+
+        <footer className="flex gap-2 items-center">
+            <Avatar>
+                <AvatarFallback className="bg-pink-300 dark:bg-pink-800">TP</AvatarFallback>
+            </Avatar>
+            <Link href="/" className="hover:underline">Logout</Link>
+            <LightDarkToggle className="ml-auto" />
+        </footer>
+
+    </nav>)
 }
---
+++ app/dashboard/employees/page.tsx
@@ -0,0 +1,9 @@
+export default function EmployeesPage() {
+    return (
+        <div>
+            <h2>
+                Employees
+            </h2>
+        </div>
+    )
+}
